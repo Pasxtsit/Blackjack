56f0f100bb2d26d701b1c9dd72f9cca58971736d
Pasxtsit <pascual_tsitsiklis@hotmail.com>
2024-05-24 16:05:37 +0300

    update bot play

--- deckAPI.js
+++ deckAPI.js
@@ -1,7 +1,7 @@
 let deckId;
 let remainingCards;
-let cardSprite;
 let handValue = [];
+let botHand = [];
 
 export function getDeckId(element) {
     element.textContent = `Deck Status`;
@@ -14,104 +14,160 @@ export function getDeckId(element) {
 
 
 export function createNewDeck(element) {
-    element.textContent = `Replay`;
-        element.addEventListener('click', () => {
-        createNewDeckInternal();
-    });
-    createNewDeckInternal();
-}
-
-function createNewDeckInternal() {
-    return fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Could not fetch the response");
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        deckId = data.deck_id;
-        remainingCards = data.remaining;
+    function setRemainingCards() {
         let remainingCardSpan = document.querySelector("#remainingCards");
         remainingCardSpan.textContent = remainingCards;
         clearValue();
-        return data.deck_id;
+    }
+
+    element.textContent = `Replay`;
+    element.addEventListener('click', () => {
+        shuffleNewDeck().then(setRemainingCards);
+    });
+    shuffleNewDeck().then(setRemainingCards);
+}
+
+
+function shuffleNewDeck() {
+    return fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Could not fetch the response");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            deckId = data.deck_id;
+            remainingCards = data.remaining;
+            return data.deck_id;
+        })
+        .catch(error => {
+            console.error(error);
+            return null;
+        });
+}
+
+export function clearValue() {
+
+    let handValueSpan = document.querySelectorAll(".handValue");
+    const cards = document.querySelectorAll(".cardClass");
+    cards.forEach(card => {
+        card.remove();
     })
-    .catch(error => {
-        console.error(error);
-        return null;
+    handValue = [];
+    handValueSpan.forEach(span=>span.textContent = 0);
+
+};
+
+export function updateUI(draw, totalHandValue, playAreaSelector) {
+    const elem =  document.querySelector(playAreaSelector);
+    remainingCards = draw.remaining;
+
+    let cardSprite = draw.cards[0].image;
+    let cardImg = document.createElement("img");
+    cardImg.setAttribute("src", cardSprite);
+    cardImg.classList.add("cardClass");
+    let remainingCardSpan = document.querySelector("#remainingCards");
+
+    remainingCardSpan.textContent = remainingCards;
+    const cardAreaElem = elem.querySelector('[data-name="cardsArea"]');
+    cardAreaElem.appendChild(cardImg);
+    let handValueSpan = elem.querySelector(".handValue");
+    handValueSpan.textContent = totalHandValue.toString();
+
+}
+
+function getHandValue(draw, hand) {
+    let cardValue = draw.cards[0].value;
+    if (cardValue === "KING" || cardValue === "QUEEN" || cardValue === "JACK") {
+        cardValue = 10;
+    } else if (cardValue === "ACE") {
+        cardValue = 11;
+    } else {
+        cardValue = parseInt(cardValue);
+    }
+    hand.push(cardValue);
+    let aces = hand.filter(value => value === 11); // filter aces in hand array
+    if (hand.length === 2 && aces.length === 2) {
+        hand = [21]; // double aces make the hand value to 21
+    } else {
+        while (hand.reduce((sum, value) => sum + value, 0) > 21 && aces.length > 0) {
+            hand[handValue.indexOf(11)] = 1;
+            aces.pop(); // Change the value of an Ace from 11 to 1 if an ace is found in hand array
+        }
+    }
+    let totalHandValue = hand.reduce((sum, value) => sum + value, 0);
+    return totalHandValue;
+}
+
+export function drawCard(numOfCards) {
+    return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Could not fetch the response");
+            }
+            return response.json();
+        }).then(data => {
+            console.log("Drawn cards:", data.cards);
+            return data;
+        });
+}
+
+export function playerDraw(element) {
+    element.textContent = `Draw a Card`;
+
+    element.addEventListener('click', () => {
+        if (!deckId) {
+            console.error("Deck ID is not provided. Please call createNewDeck() first.");
+            return;
+        }
+        drawCard(1).then(draw => {
+            const totalHandValue = getHandValue(draw, handValue);
+            updateUI(draw, totalHandValue, '#playerControls');
+            if(totalHandValue>21){
+                alert(`Burned!`)
+            }
+        })
+        .catch(error => {
+                console.error(error);
+        });
     });
 }
 
 
-export function clearValue(){
-    
-        let handValueSpan = document.querySelector("#handValue");
-        const cards = document.querySelectorAll(".cardClass");
-        cards.forEach( cards => {
-            cards.remove();})
-            handValue = [0];
-            handValueSpan.textContent = handValue.toString();
+export function passTurn(element){
+    element.textContent = `Pass Turn`;
+    element.addEventListener('click', () => {
+        let playerScore = document.querySelector('#playerControls .handValue');
+        let playerTotalHandScore = playerScore.textContent;
+        botDrawCard(playerTotalHandScore); 
+    });
+};
+
+
+async function botDrawCard(playerTotal)
+{
+    let botHandValue =0;
+    botHand=[];
+
+    do{
+        let draw = await drawCard(1);
         
-    };
-    
-    
-    export function drawCards(element) {
-        element.textContent = `Draw a Card`;
+        botHandValue = getHandValue(draw,botHand);
+        updateUI(draw, botHandValue, '#botControls');
         
-        element.addEventListener('click', () => {
-            if (!deckId) {
-                console.error("Deck ID is not provided. Please call createNewDeck() first.");
-                return;
-            }
-            fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Could not fetch the response");
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log("Drawn cards:", data.cards);
-                remainingCards = data.remaining;
-                let remainingCardSpan = document.querySelector("#remainingCards");
-                remainingCardSpan.textContent = remainingCards;
-                cardSprite = data.cards[0].image;
-                let cardImg = document.createElement("img");
-                cardImg.setAttribute("src", cardSprite);
-                cardImg.classList.add("cardClass");
-                let cardsAreaDiv = document.getElementById("cardsArea");
-                cardsAreaDiv.appendChild(cardImg);
-                let handValueSpan = document.querySelector("#handValue");
-                let cardValue = data.cards[0].value;
-                if (cardValue === "KING" || cardValue === "QUEEN" || cardValue === "JACK") {
-                    cardValue = 10;
-                } else if (cardValue === "ACE") {
-                    cardValue = 11;
-                } else {
-                    cardValue = parseInt(cardValue);
-                }
-                handValue.push(cardValue);
-                let aces = handValue.filter(value => value === 11); // filter aces in hand array
-                if (handValue.length === 2 && aces.length === 2) {
-                    handValue = [21]; // double aces make the hand value to 21
-                } else {
-                    while (handValue.reduce((sum, value) => sum + value, 0) > 21 && aces.length > 0) {
-                        handValue[handValue.indexOf(11)] = 1; 
-                        aces.pop(); // Change the value of an Ace from 11 to 1 if an ace is found in hand array
-                    }
-                }
-                handValueSpan.textContent = handValue.reduce((sum, value) => sum + value, 0).toString();
-            })
-            .catch(error => {
-                console.error(error);
-            });
-        });
+
+    }       while(botHandValue<playerTotal && botHandValue<21);
+    if (botHandValue > 21)
+        {
+            alert(`Bot is burned`)
+        }
+    else if (botHandValue >= playerTotal)
+        {
+            alert(`Bot Wins`)
+        }
+    else{
+        alert(`Player Wins`)
     }
-    
-    
-    
-    
-    
-    
+}; //end of pcdraw fun
--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 import './style.css'
 import './deckAPI.js'
 import { createNewDeck } from './deckAPI.js'
-import { drawCards } from './deckAPI.js'
+import { playerDraw } from './deckAPI.js'
+import  { passTurn } from './deckAPI.js'
 
 document.querySelector('#app').innerHTML = `
 <div class="deckDiv">
-<img class = "cardBack" src="./cardback.png"></img>
-<div class="restOfDeck"><p>Remaining Cards</p><span id="remainingCards"></span></div>
+    <img class = "cardBack" src="./cardback.png">
+    <div class="restOfDeck"><p>Remaining Cards</p><span id="remainingCards"></span></div>
 </div>
-<div>
+
 <div class="card">
-<div id="cardsArea"></div>
-<div class="lowControls"> 
-<div class="handValue"><p>Hand Value</p><span id="handValue">0</span></div>
-<button id="drawCard" type="button"></button>
-<button id="Replay" type="button"></button>
+
+<div class="playArea">
+
+<div id="playerControls">
+    <div data-name="cardsArea"></div>
+    <div><p>Hand Value</p><span class="handValue">0</span></div>
 </div>
+
+    <div id="botControls">
+        <div class="cardForPc">
+            <div data-name="cardsArea"></div>
+            <div><p>Hand Value</p><span class="handValue">0</span></div>
+        </div>
+    </div>
+
 </div>
+
+    <div class="lowControls"> 
+            <button id="drawCard" type="button"></button>
+            <button id="stop" type="button"></button>
+            <button id="Replay" type="button"></button>
+        </div>
+
+
 </div>
+
 `
 
 
-createNewDeck(document.querySelector('#Replay'))
-drawCards(document.querySelector('#drawCard'))
-
+createNewDeck(document.querySelector('#Replay'));
+playerDraw(document.querySelector('#drawCard'));
+passTurn(document.querySelector('#stop'))
 
 
 
